f4f013f32edde13ca3bd333d1c4183de58a75797
Venkat K <karipireddy@gmail.com>
2015-05-20 15:44:15 -0400

    Category Schema added

---
+++ app/models/category.server.model.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Category Schema
+ */
+var CategorySchema = new Schema({
+    catId:{
+		type: String,
+		default: '',
+		trim: true,
+		required: 'catid cannot be blank'
+	},
+	displayName: {
+		type: String,
+		default: '',
+		trim: true,
+		required: 'displayName cannot be blank'
+	},
+	description: {
+		type: String,
+		default: '',
+		trim: true
+	},
+    imageName: {
+		type: String,
+		default: ''
+	},
+    status: {
+		type: String,
+		default: 'N'
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	},
+  	created: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+mongoose.model('Category', CategorySchema);
