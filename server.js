a2e87e8201ae6f6ea728b3dbabd639a35ccf5e9d
Venkat K <karipireddy@gmail.com>
2015-06-11 17:24:48 -0400

    adding config changes for secure env

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
  * Module dependencies.
  */
 var init = require('./config/init')(),
-	config = require('./config/config'),
-	mongoose = require('mongoose'),
-	chalk = require('chalk'),
+  config = require('./config/config'),
+  mongoose = require('mongoose'),
+  chalk = require('chalk'),
   bodyParser = require('body-parser');
 
 /**
@@ -14,17 +14,16 @@ var init = require('./config/init')(),
  */
 
 // Bootstrap db connection
-var db = mongoose.connect(config.db.uri, config.db.options, function(err) {
-	if (err) {
-		console.error(chalk.red('Could not connect to MongoDB!'));
-		console.log(chalk.red(err));
-	}
+var db = mongoose.connect(config.db.uri, config.db.options, function (err) {
+  if (err) {
+    console.error(chalk.red('Could not connect to MongoDB!'));
+    console.log(chalk.red(err));
+  }
 });
-mongoose.connection.on('error', function(err) {
-	console.error(chalk.red('MongoDB connection error: ' + err));
-	process.exit(-1);
-	}
-);
+mongoose.connection.on('error', function (err) {
+  console.error(chalk.red('MongoDB connection error: ' + err));
+  process.exit(-1);
+});
 
 // Init the express application
 var app = require('./config/express')(db);
@@ -37,7 +36,7 @@ app.listen(config.port);
 
 
 // parse application/json
-app.use(bodyParser.json());
+//app.use(bodyParser.json()); // this is already done in the config/express.js so commenting
 
 // Expose app
 exports = module.exports = app;
@@ -49,6 +48,6 @@ console.log(chalk.green('Environment:\t\t\t' + process.env.NODE_ENV));
 console.log(chalk.green('Port:\t\t\t\t' + config.port));
 console.log(chalk.green('Database:\t\t\t' + config.db.uri));
 if (process.env.NODE_ENV === 'secure') {
-	console.log(chalk.green('HTTPs:\t\t\t\ton'));
+  console.log(chalk.green('HTTPs:\t\t\t\ton'));
 }
 console.log('--');
