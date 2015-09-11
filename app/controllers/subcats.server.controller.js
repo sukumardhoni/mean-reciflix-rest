921d07bdd68c662508584a804581a7bb0820946e
Venkat K <karipireddy@gmail.com>
2015-09-10 23:38:16 -0400

    image ext changed to jpg

--- app/controllers/subcats.server.controller.js
+++ app/controllers/subcats.server.controller.js
@@ -64,9 +64,7 @@ exports.listOfSubCats = function (req, res) {
           message: errorHandler.getErrorMessage(err)
         });
       } else if (subcats.length === 0) {
-        return res.status(204).send({
-          message: 'No data found'
-        });
+          res.json(catResult);
       } else {
         catResult.subCats = subcats;
         //user is successfully fetched list of sub cats save action into user usage details collection
@@ -90,44 +88,9 @@ exports.listOfSubCats = function (req, res) {
           message: errorHandler.getErrorMessage(err)
         });
       } else if (subcats.length === 0) {
-
-      /*  Vrecipe.find({
-          cats: {
-            $in: [req.params.newCatId]
-          }
-        }).sort({
-          rank: -1
-        }).skip(req.params.pageId * 5).limit(5).exec(function (err, recipes) {
-          if (!err) {
-            if ((recipes.length === 0)) {
-              res.status(204).send({
-                'message': 'There are no recipe items available'
-              });
-            } else {
-              //user is successfully fetched list of recipes based on categories save action into user usage details collection
-              agenda.now('User_Usage_Details', {
-                email: emailInfo,
-                device: deviceInfo,
-                action: 'getVIdRecipesByCategories : ' + req.params.CategoryName
-              });
-              catResult.recipes = recipes;
-              //console.log('Recipes length is : ' + recipes.length);
-              res.send(catResult);
-            }
-          } else {
-            return console.log(err);
-          }
-        });*/
-        /*return res.status(204).send({
-          message: 'No data found'
-        });*/
         res.json(catResult);
       } else {
-        //console.log('Before pushing the subcats into cats obj : ' + JSON.stringify(catResult));
-        //console.log('@@@@@@@@@@@@@@@@@@@@@');
-        //console.log(' subcats into cats obj : ' + JSON.stringify(subcats));
         catResult.subCats = subcats;
-        //console.log('After pushed the subcats into cats obj : ' + JSON.stringify(catResult));
         //user is successfully fetched list of sub cats save action into user usage details collection
         agenda.now('User_Usage_Details', {
           email: emailInfo,
