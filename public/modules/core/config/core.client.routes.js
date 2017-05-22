43bb096b6442603b6fe1fac4c74e44a3eb66eca0
Vinodh <vinodhko@globaltechminds.com>
2017-05-22 14:22:02 +0530

    Included the privacy policy

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -3,30 +3,34 @@
 // Setting up route
 angular.module('core').config(['$stateProvider', '$urlRouterProvider',
  function ($stateProvider, $urlRouterProvider) {
-    // Redirect to home view when route not found
-    $urlRouterProvider.otherwise('/home');
+		// Redirect to home view when route not found
+		$urlRouterProvider.otherwise('/home');
 
-    // Home state routing
-    $stateProvider
-      .state('home', {
-        url: '/home',
-       templateUrl: 'modules/core/views/home.client.view.html',
-        data: {
-          bodyClass: 'bg-body'
-        }
-      })/*
-      .state('terms', {
-        url: '/terms',
-        templateUrl: 'modules/core/views/terms.client.view.html',
-        module: 'public'
-      })
-      .state('privacy', {
-        url: 'privacy',
-        templateUrl: 'modules/core/views/privacy.client.view.html',
-        module: 'public'
-      });*/
+		// Home state routing
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: 'modules/core/views/home.client.view.html',
+				data: {
+					bodyClass: 'bg-body'
+				}
+			})
+			/*
+			      .state('terms', {
+			        url: '/terms',
+			        templateUrl: 'modules/core/views/terms.client.view.html',
+			        module: 'public'
+			      })*/
+			.state('privacy', {
+				url: '/privacy',
+				templateUrl: 'modules/core/views/privacy.client.view.html',
+				module: 'public',
+				data: {
+					bodyClass: ''
+				}
+			});
  }
 ]).run(function ($rootScope, $state, $stateParams) {
-  $rootScope.$state = $state;
-  $rootScope.$stateParams = $stateParams;
+	$rootScope.$state = $state;
+	$rootScope.$stateParams = $stateParams;
 });
