6510c9569f5539920ccf63f0f65728bdb6e699fc
Vinodh <vinodhko@globaltechminds.com>
2017-07-24 14:25:07 +0530

    changed send push notifications

--- public/dist/application.js
+++ public/dist/application.js
@@ -1845,6 +1845,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 
  $scope.sendNotifications = function (webNotification) {
       console.log("VVBB@@@ : " + JSON.stringify(webNotification))
+      if(webNotification.url){
       var notificationObj = {
         title: webNotification.title,
         message: webNotification.msg,
@@ -1858,63 +1859,11 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
       }, function errCalBck(err) {
         console.log("error of sending notification : " + JSON.stringify(err))
       })
-    }
+      }
+    
+ }
 
-
- }]).directive('showButton', ['webNotification', function (webNotification) {
-return {
-    restrict: 'C',
-    scope: {
-      notificationTitle: '=',
-      notificationMsg: '=',
-      notificationUrl: '='
-    },
-    link: function (scope, element) {
-      console.log("coming to directive notifications")
-      element.on('click', function onClick() {
-        console.log("coming to directive notifications click")
-
-        if ((scope.notificationTitle) && (scope.notificationMsg)) {
-          var notification = new Notification(scope.notificationTitle, {
-            body: scope.notificationMsg,
-            icon: 'https://lh3.googleusercontent.com/BCOE0vqCfr8aqpIKEF7QEt-qa7p8I7KDg58Juz6M6_YGb4l7phrO2vMvi_SDy10ucQ=w300'
-
-          });
-
-
-          notification.onclick = function () {
-            console.log("####")
-             window.open(scope.notificationUrl, '_blank')
-          }
-          setTimeout(function () {
-            notification.close();
-          }, 5000);
-
-          /*  webNotification.showNotification(scope.notificationTitle, {
-              body: scope.notificationMsg,
-              icon: 'https://lh3.googleusercontent.com/BCOE0vqCfr8aqpIKEF7QEt-qa7p8I7KDg58Juz6M6_YGb4l7phrO2vMvi_SDy10ucQ=w300',
-              onClick: function onNotificationClicked() {
-                console.log('Notification clicked.');
-                window.open(scope.notificationUrl, '_blank')
-              },
-              autoClose: 4000 //auto close the notification after 4 seconds (you can manually close it via hide function)
-            }, function onShow(error, hide) {
-              if (error) {
-                window.alert('Unable to show notification: ' + error.message);
-              } else {
-                console.log('Notification Shown.');
-
-                setTimeout(function hideNotification() {
-                  console.log('Hiding notification....');
-                  hide(); //manually close the notification (you can skip this if you use the autoClose option)
-                }, 5000);
-              }
-            });*/
-        }
-      });
-    }
-  };
-}]);
+ }]);
 
 /**
  * INSPINIA - Responsive Admin Theme
