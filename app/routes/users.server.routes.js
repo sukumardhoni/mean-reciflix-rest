5e79e8e198298c7d3b9cf6ddb2ac06fc220f2554
Vinodh <vinodhko@globaltechminds.com>
2017-07-25 12:18:19 +0530

    Package.json updated

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -8,7 +8,7 @@ var passport = require('passport');
 module.exports = function (app) {
 	// User Routes
 	var users = require('../../app/controllers/users.server.controller');
-
+ 
 	// Setting up the users profile api
 	app.route('/users/me').get(users.me);
 
